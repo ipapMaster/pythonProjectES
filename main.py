a8b795236f801aab760fbf88ad10118878814478
man <a@b.ru>
2023-07-04 16:47:13 +0300

    JSON Load and Parse

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-import json  # Java Script Object Notation
+import requests  # библиотека для http-запросов
+from datetime import datetime
 
-with open('pets.json') as pet_file:
-    data = json.load(pet_file)
+town = input('Введите город: ')
 
-for item in data:
-    if type(data[item]) == list:
-        print(item, ', '.join(data[item]))
-    elif type(data[item]) == dict:
-        print(item)
-        for k, v in data[item].items():
-            print(k, v)
-    else:
-        print(item, data[item])
+key = 'c747bf84924be997ff13ac5034fa3f86'
+url = 'http://api.openweathermap.org/data/2.5/weather'
+params = {'APPID': key, 'q': town, 'units': 'metric'}
+
+request = requests.get(url, params=params)
+
+result = request.json()
+print(result['main']['feels_like'])
+code = result['cod']
+
+if code != '401':
+    if code != '404':
+        data = result['main']
+        print(f'Температура: {data["temp"]:.1f}\xB0C')
+        print('Влажность', data['humidity'], '%')
+        raw_time_sunset = result['sys']['sunset']
+        print('Закат:', datetime.utcfromtimestamp(raw_time_sunset).strftime("%H:%M"))
+        print(f'Координаты: {result["coord"]["lon"]}, {result["coord"]["lat"]}')
+        if result['weather'][0]['main'] == 'Clouds':
+            print('Облачно')
+else:
+    print('Информации нет!')
