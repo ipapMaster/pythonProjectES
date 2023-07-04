87d64ffe099fbcd86898c3d6bf23aa0b1541cd83
man <a@b.ru>
2023-07-04 14:51:31 +0300

    JSON Dumps

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# Начало проекта
+import json  # Java Script Object Notation
+
+weekdays = ['Понедельник', 'Вторник', 'Среда', 'Четверг',
+            'Пятница', 'Суббота', 'Воскресенье']
+
+
+week_dict = {key: val for key, val in enumerate(weekdays)}
+
+data = json.dumps(week_dict)
+
+print(data)
